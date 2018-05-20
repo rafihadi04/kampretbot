6c024a522d23e4f3716b183b74e5125cff00a9a8
rafihadi04 <32604312+rafihadi04@users.noreply.github.com>
2018-05-20 21:21:43 +0800

    Update LineTransport.py

--- LineAlpha/LineApi/LineTransport.py
+++ LineAlpha/LineApi/LineTransport.py
@@ -20,9 +20,8 @@ except:
 
 class LineTransport(TTransportBase):
 
-    UserAgent   = 'DESKTOP:MAC:10.10.2-YOSEMITE-x64(4.5.0)'
-    AppName     = 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0'
-
+    UserAgent   = 'Line/7.14.0'
+    AppName     = 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0'
     def __init__(self, uri_or_host, port=None, path=None):
         if port is not None:
             warnings.warn(
